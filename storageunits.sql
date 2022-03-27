e0225f508efe26f5bbf6d8ad175bb1a11f76c459
KicoOriginal <102483355+KicoOriginal@users.noreply.github.com>
2022-03-27 20:31:31 +0200

    Update storageunits.sql
    Saving items on server restart

--- storageunits.sql
+++ storageunits.sql
@@ -2,21 +2,22 @@ CREATE TABLE IF NOT EXISTS `storageunits` (
   `id` int(11) NOT NULL,
   `identifier` varchar(250) DEFAULT NULL,
   `pin` longtext DEFAULT NULL,
+  `stashsave` longtext DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `storageunits` (`id`, `identifier`, `pin`) VALUES
-	(1, NULL, NULL),
-	(2, NULL, NULL),
-	(3, NULL, NULL),
-	(4, NULL, NULL),
-	(5, NULL, NULL),
-	(6, NULL, NULL),
-	(7, NULL, NULL),
-	(8, NULL, NULL),
-	(9, NULL, NULL),
-	(10, NULL, NULL),
-	(11, NULL, NULL),
-	(12, NULL, NULL),
-	(13, NULL, NULL),
-	(14, NULL, NULL);
+INSERT INTO `storageunits` (`id`, `identifier`, `pin`, `stashsave`) VALUES
+	(1, NULL, NULL, '999999'),
+	(2, NULL, NULL, '999998'),
+	(3, NULL, NULL, '999997'),
+	(4, NULL, NULL, '999996'),
+	(5, NULL, NULL, '999995'),
+	(6, NULL, NULL, '999994'),
+	(7, NULL, NULL, '999993'),
+	(8, NULL, NULL, '999992'),
+	(9, NULL, NULL, '999991'),
+	(10, NULL, NULL, '999990'),
+	(11, NULL, NULL, '999989'),
+	(12, NULL, NULL, '999988'),
+	(13, NULL, NULL, '999987',
+	(14, NULL, NULL, '999986');
